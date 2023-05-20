e1428c3ce55bbb1d5fbd203fa6b251d090397ba6
fifth gay rat <124536735+fdsa-rewq@users.noreply.github.com>
2023-05-20 13:48:47 -0500

    added blogpost by marshie

--- blog.js
+++ blog.js
@@ -18,6 +18,15 @@ fetch('./posts/posts.json')
             <img src="${entry.picture}" alt="${entry.picturealt}" class="blogPostImage">
             <p>${entry.contents}</p>
           `;
+          } else if (entry.bottompicture) {
+            entryElement.innerHTML = `
+            <h2>${entry.name}</h2>
+            <h3><i>By ${entry.author}</i></h3>
+            <p>Date: ${entry.date}</p>
+            <hr>
+            <p>${entry.contents}</p>
+            <p>img src="${entry.bottompicture}" alt="${entry.picturealt}" class="blogPostImageBottom"</p>
+          `;
           } else {
             entryElement.innerHTML = `
             <h2>${entry.name}</h2>
